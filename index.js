9d66a922afd2f8444a6edec44e27893de1e9fdac
Bashir200 <musubirebashir@gmail.com>
2020-02-07 19:24:35 +0300

    updateded files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const path = require('path')
 
 const app = express()
 
@@ -8,9 +10,25 @@ app.listen(3000, () =>{
 	console.log("images running on port 3000...")
 })
 
+
+const viewsDir = path.join(__dirname, '.')
+
+/*const corsOptions = {
+  origin: 'https://github.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}*/
+
+app.use(cors())
 app.use(express.static('.'))
 app.use(express.static('weights'))
 
+/*app.get('/', cors(), function (req, res, next) {
+  console.log('yap')
+  res.sendFile('index1.html')
+  //res.json({msg: 'This is CORS-enabled for a Single Route'})
+})*/    
+            //cors(corsOptions)
+app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index1.html')))
 /*const url = 'http://127.0.0.1:8008';
 
     axios(url)
